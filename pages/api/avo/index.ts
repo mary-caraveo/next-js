e25bee16f75f69ef8e5b18fd0ccba29086020449
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-11-12 16:12:26 -0500

    feat: CÃreación deAPI con NextJS

---
+++ pages/api/avo/index.ts
@@ -0,0 +1,16 @@
+import { IncomingMessage, ServerResponse } from 'http';
+import DB from '@database';
+
+const allAvos = async (request: IncomingMessage, response: ServerResponse) => {
+
+    const db = new DB();
+    const allEntries = await db.getAll();
+    const length = allEntries.length;
+
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'application/json')
+    response.end(JSON.stringify({ length, data: allEntries }))
+
+}
+
+export default allAvos;
