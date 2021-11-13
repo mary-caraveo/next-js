dba4280721c84fa46327aa4085d0e407ee197a55
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-11-13 12:34:18 -0500

    feat: Creando y consumiendo nuestra propia API

--- pages/api/avo/index.ts
+++ pages/api/avo/index.ts
@@ -9,8 +9,7 @@ const allAvos = async (request: IncomingMessage, response: ServerResponse) => {
 
     response.statusCode = 200
     response.setHeader('Content-Type', 'application/json')
-    response.end(JSON.stringify({ length, data: allEntries }))
-
+    response.end(JSON.stringify({ data: allEntries, length }))
 }
 
 export default allAvos;
---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Navbar from '../components/Navbar/Navbar';
+
+const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([]);
+
+  useEffect(() => {
+    window
+      .fetch('/api/avo')
+      .then(response => response.json()
+      .then(({ data, length }) => {
+          setProductList(data)
+        }));
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <h1>Next JS</h1>
+      {productList.map((product) => (
+        <div>{product.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default HomePage;
