1f35e600ea95a76ed1f5661fd1689fe949a65fc3
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-11-24 12:40:03 -0500

    style: scambio de estilos al footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,56 +1,78 @@
 import React from 'react';
 import Link from 'next/link';
-import { Segment, Container, Grid, List, Header } from 'semantic-ui-react';
+import { Segment, Container, Grid, List } from 'semantic-ui-react';
 
 const Footer = () => (
   <Segment
     vertical
     as="footer"
     style={{
-      padding: "4em 0em",
+      padding: "1.5em 0em",
       marginTop: "3em",
       borderTop: "1px solid #f2f2f2",
+      background: "#2c2b29",
     }}
   >
     <Container text>
       <Grid stackable>
         <Grid.Row>
-          <Grid.Column width={4}>
-            <Header as="h4" content="Nosotros" />
+          <Grid.Column width={5}>
             <List>
               <List.Item>
                 <Link href="/about">
-                  <a>Conoce más</a>
+                  <a
+                  style={{
+                    color:"white"
+                  }}
+                  >Nosotros</a>
+                </Link>
+              </List.Item>
+            </List>
+          </Grid.Column>
+          <Grid.Column width={6}>
+          <List>
+              <List.Item>
+                <Link href="/">
+                  <a
+                    style={{
+                      color:"white"
+                    }}
+                  >Servicios</a>
                 </Link>
               </List.Item>
             </List>
           </Grid.Column>
           <Grid.Column width={5}>
-            <Header as="h4" content="Servicios" />
-            <List>
+          <List>
               <List.Item>
                 <Link href="/">
-                  <a>Todos los productos</a>
+                  <a
+                    style={{
+                      color:"white"
+                    }}
+                    >Contactanos</a>
                 </Link>
               </List.Item>
             </List>
-          </Grid.Column>
-          <Grid.Column width={7}>
-            <Header as="h4">Contactanos</Header>
           </Grid.Column>
         </Grid.Row>
       </Grid>
     </Container>
 
-    <style jsx>{`
+    <style jsx global>{`
       .colophon {
         text-align: center;
         margin-top: 3.2rem;
         font-size: 0.8rem;
       }
+
       .colophon-entry {
         color: grey;
         margin-bottom: 0;
+      }
+
+      .ui.grid>.row>.column {
+        text-align: center;
       }
     `}</style>
   </Segment>
