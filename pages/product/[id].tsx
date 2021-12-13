559dc6de4cc1e40803ab20afd1e547a647e016a6
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-12-13 16:41:44 -0500

    feat: #UnderTheHood Static Dynamic Static Generation: getStaticPaths

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,22 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import React from 'react';
 import Layout from '@components/Layout/Layout';
 import ProductSummary from '@components/ProductSummary/ProductSummary';
 import fetch from 'isomorphic-unfetch';
+import { GetStaticProps } from 'next';
 
-const ProductPage = () => {
-  const { query } = useRouter()
-  const [product, setProduct] = useState<TProduct | null>(null)
+export const getStaticPaths = async () => {
+  const response = await fetch('https://next-js-sand-tau-80.vercel.app/api/avo');
+  const { data: productList }: TAPIAvoResponse = await response.json();
 
-  useEffect(() => {
-    if (query.id) {
-        fetch(`/api/avo/${query.id}`)
-        .then((response) => response.json())
-        .then((data: TProduct) => {
-          setProduct(data)
-        })
+  const paths = productList.map(({ id }) => ({
+    params: {
+      id
     }
-  }, [query.id])
+
+  }))
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id = params?.id as string;
+  const response = await fetch(`https://next-js-sand-tau-80.vercel.app/api/avo/${id}`);
+  const product :  TProduct  = await response.json();
+
+  return {
+    props: {
+      product,
+    }
+  }
+}
+
+const ProductPage = ({ product }: { product: TProduct }) => {
 
   return (
     <Layout>
