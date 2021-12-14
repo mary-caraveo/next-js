8f1771a6f46790bb39dc73b12cd5f41dc42e7474
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-12-14 11:25:09 -0500

    feat: Mide Performance en NextJS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
-import { AppProps } from 'next/app';
+import { AppProps, NextWebVitalsMetric } from 'next/app';
 import CartProvider from '@store/Cart/Cart';
 import 'semantic-ui-css/semantic.min.css';
 import '../global.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <CartProvider>
-      <Component {...pageProps} />
-    </CartProvider>
-  )
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  console.log(metric)
 }
 
-export default MyApp;
+  const MyApp = ({ Component, pageProps }: AppProps) => {
+    return (
+      <CartProvider>
+        <Component {...pageProps} />
+      </CartProvider>
+    )
+  }
+
+  export default MyApp;
