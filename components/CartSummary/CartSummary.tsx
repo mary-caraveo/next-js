ea73277eb23369398725c9aa6b6e9a5b181c0d5c
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-12-15 23:29:33 -0500

    style: hover al botón de pagar

--- components/CartSummary/CartSummary.tsx
+++ components/CartSummary/CartSummary.tsx
@@ -2,26 +2,42 @@ import React from 'react';
 import { Segment, Button } from 'semantic-ui-react';
 
 type CartSummaryProps = {
-  totalAmount: number;
-  quantityProducts: number;
-};
+  totalAmount: number
+  quantityProducts: number
+}
 
 const CartSummary = ({ totalAmount, quantityProducts }: CartSummaryProps) => {
   return (
     <Segment clearing size="large" as="section">
-      <span style={{display: 'flex', paddingBottom: '25px', fontSize: '16px'}}>
-        <strong style={{paddingRight: '25px'}}>Cantidad: </strong>
+      <span
+        style={{ display: 'flex', paddingBottom: '25px', fontSize: '16px' }}
+      >
+        <strong style={{ paddingRight: '25px' }}>Cantidad: </strong>
         {`${quantityProducts}`}
       </span>
       <span>
-        <strong style={{paddingRight: '45px', fontSize: '16px'}}>Total: </strong>
+        <strong style={{ paddingRight: '45px', fontSize: '16px' }}>
+          Total:{' '}
+        </strong>
         ${`${totalAmount}`}.00
       </span>
-      <Button style={{fontSize: '14px', backgroundColor: '#0c7825', color: 'white'}} floated="right">
+      <Button
+        style={{ fontSize: '14px', backgroundColor: '#0c7825', color: 'white' }}
+        floated="right"
+      >
         Pagar
       </Button>
-    </Segment>
-  );
-};
 
-export default CartSummary;
+      <style jsx global>
+        {`
+          .ui.button:hover {
+            transform: scale(1.1);
+            font-size: 14px;
+          }
+        `}
+      </style>
+    </Segment>
+  )
+}
+
+export default CartSummary
